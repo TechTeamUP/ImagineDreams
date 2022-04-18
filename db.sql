abc8cc8385aaaa337e8d556141f11e87f199a0a9
DevWill <willintong9612@gmail.com>
2022-04-18 17:07:23 -0500

    Adding the db file

---
+++ db.sql
@@ -0,0 +1,73 @@
+#Creación de la base de datos.
+DROP DATABASE testing;
+CREATE DATABASE IF NOT EXISTS testing;
+
+#Creación de las tablas.
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    adress VARCHAR(255) NOT NULL,
+    phone VARCHAR(30) NOT NULL,
+    birth DATE NOT NULL,
+    type VARCHAR(20) DEFAULT 'CUSTOMER',
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS product(
+    product_id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    category  VARCHAR(50) NOT NULL,
+    price VARCHAR(255) NOT NULL,
+    stock INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(category_id) REFERENCES category(category_id)
+);
+
+CREATE TABLE IF NOT EXISTS shopping_cart(
+    id INT AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    quantity  INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+CREATE TABLE IF NOT EXISTS invoice(
+    id INT AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    user_id  INT NOT NULL,
+    quantity INT NOT NULL,
+    total_price INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+CREATE TABLE IF NOT EXISTS invoice(
+    id INT AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    user_id  INT NOT NULL,
+    quantity INT NOT NULL,
+    total_price INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (product_id) REFERENCES productxshopping_cart(product_id)
+);
+CREATE TABLE IF NOT EXISTS productxshopping_cart(
+    id INT AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    user_id  INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS category(
+	category_id INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (category_id)
+);
+
+
+
+
