75df78afe43916ea1646729b6604be66a716ad13
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-04-25 16:15:11 -0500

    Creation of the search page and connection of home with the item page

---
+++ ClientApp/src/app/interfaces/item-product.interface.ts
@@ -0,0 +1,10 @@
+export interface ProductDescription {
+  author?: string;
+  category?: string;
+  description?: string;
+  fec_publ?: string;
+  img_aut?: string;
+  price?: number;
+  title?: string;
+  url_img?: string;
+}
