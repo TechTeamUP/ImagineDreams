7b7272e531853595138c7b0e72c16593abac87b3
JeisonMC <jeisongmc2020@gmail.com>
2022-04-27 11:35:31 -0500

    connection with firebase

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,15 +4,24 @@ import { HomeComponent } from "./components/pages/home/home.component";
 import { AboutComponent } from './components/pages/about/about.component';
 import { SearchComponent } from './components/pages/search/search.component';
 import { ItemDetailsComponent } from './components/pages/item-details/item-details.component';
+import { ImagesComponent } from "./images/images.component";
+import { ImageListComponent } from "./images/image-list/image-list.component";
 
 
 
 const app_routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'item/:id', component: ItemDetailsComponent},
-    {path: 'search/:term', component: SearchComponent},
-    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item/:id', component: ItemDetailsComponent },
+    { path: 'search/:term', component: SearchComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+    { path: '', redirectTo: 'image/upload', pathMatch: 'full' },
+    {
+        path: 'image', component: ImagesComponent, children: [
+            { path: 'upload', component: ImagesComponent },
+            { path: 'lsit', component: ImageListComponent }
+        ]
+    }
 ];
 
 @NgModule({
