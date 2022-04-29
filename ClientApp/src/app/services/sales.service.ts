cde946a0aee752041a882a6ae0d3c7a37ad32e2b
JeisonMC <jeisongmc2020@gmail.com>
2022-04-28 21:54:21 -0500

    rama recuperada

--- ClientApp/src/app/services/sales.service.ts
+++ ClientApp/src/app/services/sales.service.ts
@@ -6,13 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class SalesService {
 
-  
   private salesURL = "https://localhost:7200/service/sales/create"
 
   constructor(private http: HttpClient) { }
 
-  sales(body: any)  
-  {
-    return this.http.post(this.salesURL , body)
+  sales(body: any) {
+    return this.http.post(this.salesURL, body)
   }
 }
