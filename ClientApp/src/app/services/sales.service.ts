13b835a68dff853896b6d04d2f63d248d540ea6b
JeisonMC <jeisongmc2020@gmail.com>
2022-05-02 15:19:29 -0500

    ajust service create sales

--- ClientApp/src/app/services/sales.service.ts
+++ ClientApp/src/app/services/sales.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
 
-  
+
   private salesURL = "https://localhost:7200/service/sales/create"
 
   constructor(private http: HttpClient) { }
 
-  sales(body: any) {
+  sales(body: any): Observable<any> {
     return this.http.post(this.salesURL, body)
   }
 }
